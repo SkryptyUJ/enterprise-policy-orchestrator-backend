46d41124c297f482328ad31e3ed056c1232aae06
Oliwia Gierlicka <oliwia.gierlicka@student.uj.edu.pl>
2026-03-26 18:06:11 +0100

    Create endpoint for saving users finance request

---
+++ src/main/java/com/uj/enterprise_policy_orchestrator/controller/ExpenseRequestController.java
@@ -0,0 +1,23 @@
+package com.uj.enterprise_policy_orchestrator.controller;
+
+import com.uj.enterprise_policy_orchestrator.dto.CreateExpenseRequestDto;
+import com.uj.enterprise_policy_orchestrator.dto.ExpenseRequestDto;
+import com.uj.enterprise_policy_orchestrator.service.ExpenseRequestService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/users/{userId}/expense-requests")
+@RequiredArgsConstructor
+public class ExpenseRequestController {
+
+  private final ExpenseRequestService expenseRequestService;
+
+  @PostMapping
+  @ResponseStatus(HttpStatus.CREATED)
+  public ExpenseRequestDto createExpenseRequest(
+      @PathVariable Long userId, @RequestBody CreateExpenseRequestDto dto) {
+    return expenseRequestService.createExpenseRequest(userId, dto);
+  }
+}
